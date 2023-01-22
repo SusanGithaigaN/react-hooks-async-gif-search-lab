fc73177f99bb97585e21ef919153a91625d36b0c
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-01-22 09:22:23 +0300

    Completed Assignment

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,7 +2,7 @@ function GifList({gifs}){
     return (
         <ul className="GifList container">
            {gifs.map((gif) => (
-            <li className="my-gifs" key={gif}>
+            <li className="my-gifs" key={gif.id}>
             <img src={gif.url} alt="gif-here" />
             </li>
            ))}
--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,21 +1,25 @@
 import React from "react";
 import GifSearch from "./GifSearch";
 import GifList from "./GifList";
-import "./App.css";
+// import "./App.css";
 import { useState, useEffect } from "react";
 
 function GifListContainer (){
     const[gif, setGif] = useState([])
     const[search, setSearch] = useState("")
+    // assign personal API key to a variable
+    const myApiKey= "8dnjIQswNVaihW9B296nmf9g9EfBtIZg";
 
     // fire a function to begin with once the component first loads on the page
     useEffect(()=>{
-        fetch("https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g")
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${myApiKey}&rating=g`)
         .then(response => response.json())
+        // .then((data)=> setGif(data))
         .then(({data}) => {
-            const gifs = data.slice(0,3).map((gif)=>({url: gif.images.orignal.url }));
+            const gifs = data.slice(0,3).map((gif)=>({url: gif.images.original.url }));
             setGif(gifs);
         })
+        
     // pass the search variable as the dependecy to run the user search when the function changes
     }, [search])
 
@@ -24,7 +28,6 @@ function GifListContainer (){
         <>
             <GifSearch search={setSearch} />
             <GifList gifs={gif}/>
-
         </>
     )
 }
